afed53937aa1f395a17b5bbfe43dee418086c135
ashraf moinuddin <dota2khelbo@gmail.com>
2018-08-12 23:27:00 +0600

    version 1.0

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApiClient;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SampleModel sample = new SampleModel();
+            sample = APIFactory<SampleModel>.GetJSONData("https://jsonplaceholder.typicode.com/todos/3");
+            Console.WriteLine(sample.title);
+        }
+    }
+}
---
+++ WebApiClient/APIFactory.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApiClient
+{
+    public static class APIFactory<T> where T:class
+    {
+        public static T GetJSONData(string url)
+        {
+            T data;
+            HttpWebRequest request = WebRequest.Create(url) as HttpWebRequest;
+            request.ContentType = "application / json";
+            request.Method = "GET";
+
+            using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)
+            {
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                    using (StreamReader reader = new StreamReader(response.GetResponseStream()))
+                    {
+                        string content = reader.ReadToEnd();
+                        data = JsonConvert.DeserializeObject<T>(content);
+                    }
+                    return data;
+                }
+                else
+                    return null;
+            }
+        }
+
+    }
+}
